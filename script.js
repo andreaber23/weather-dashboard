3f89d42be01e65a0df0f4f1507cd0b3fc7baaa72
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-05-13 20:21:10 -0400

    results are not display. There might be a CORS issue. Changed the url to see if it would work fine and give a 5 day forecast

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-var WeatherApiKey = '7bd88fc5034f484139c0ae09d042d15c';
+var weatherApiKey = '7bd88fc5034f484139c0ae09d042d15c';
 
-function getForecast(event) {
-    event.preventDefault();
-    const apiUrl ='api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
-    const city = document.getElementById('city').value;
+function getForecast() {
+  
+    const newCity = document.getElementById("cityInput").value
+    const cityName = document.getElementById('cityName');
+    cityName.textContent = newCity
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(newCity)}&appid=7bd88fc5034f484139c0ae09d042d15c&cnt=5`
 
     fetch(apiUrl)
         .then(response => response.json())
         .then (data => {
+            
+            if (data.cod ===200){
             displayForecast(data);
-            searchHistory(city)
-        })
+            searchHistory(data.city.name)
+} else {
+    throw new Error(data.message);
+}
+})
         .catch(error => {
             console.log ('Error', error);
-            document.getElementById('forecast').innerHTML='Error displaying forecast'
+            document.getElementById('forecast').innerHTML='Error displaying forecast';
         })
+        
 }
-
 function displayForecast(data) {
     const forecast = document.getElementById('forecast');
     forecast.innerHTML = '';
 
-    const cityName = document.createElement('h2');
-    cityName.textContent= document.getElementById('city').value ;
+    const cityName = document.getElementById('cityName');
+    cityName.textContent= data.city.name ;
     forecast.appendChild(cityName);
 
+    const temperatureKelvin = data.list[0].main.temp;
+    const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9/5 + 32;
     const temperature = document.createElement('p');
-    temperature.innerHTML = 'Temperature: ${data.list[0].main.temp}' ; 
-    forecast.appendchild(temperature);
+    temperature.innerHTML = `Temperature: ${temperatureFahrenheit.toFixed(2)} °F`
+    forecast.appendChild(temperature);
 
     const wind = document.createElement('p');
-    wind.innerHTML = 'Wind: ${data.list[0].wind.speed} m/s';
+    wind.innerHTML = `Wind: ${data.list[0].wind.speed} m/s`;
     forecast.appendChild(wind);
 
     const humidity = document.createElement('p');
-    humidity.innerHTML= 'Humidity ${data.list[0].main.humidity}%';
+    humidity.innerHTML= `Humidity: ${data.list[0].main.humidity}%`;
     forecast.appendChild(humidity);
+
 }
 
 function searchHistory(city) {
@@ -44,3 +54,12 @@ function searchHistory(city) {
     searchItem.textContent = city;
     searchHistory.appendChild(searchItem);
 }
+document.addEventListener("DOMContentLoaded", () => {
+    
+    document.querySelector("#search-btn").addEventListener("click", () => {
+        const searchedCity = document.querySelector('#cityInput');
+        console.log(searchedCity.value);
+        if(searchedCity.value){
+            getForecast(searchedCity.value);
+        }
+    })})
